8c95566fa7042bd066187f457f832cfda44fcdd7
el sedy ba <elsedy000@gmail.com>
2023-02-23 02:06:41 +0000

    importation de quelque bibliotheque

--- python-scripts/script.py
+++ python-scripts/script.py
@@ -1,18 +1,20 @@
 import sys
 import os
+from builtins import print, len, str, int
 
 workingDirectory = os.getcwd()
-print('Number of Arguments:' , len(sys.argv), 'arguments')
-print('Argument List:',str(sys.argv))
+print('Number of Arguments:', len(sys.argv), 'arguments')
+print('Argument List:', str(sys.argv))
 
 
 def addition():
     print(sys.argv[1])
-    num1=sys.argv[1]
-    num2=sys.argv[2]
-    numbers=[num1,num2]
+    num1 = sys.argv[1]
+    num2 = sys.argv[2]
+    numbers = [num1, num2]
     print(numbers)
-    sumof=int(num1)+int(num2)
-    print("Sum of {0} and {1} is {2}".format(num1,num2,sumof))
+    sum_of = int(num1) + int(num2)
+    print("Sum of {0} and {1} is {2}".format(num1, num2, sum_of))
+
 
 addition()
